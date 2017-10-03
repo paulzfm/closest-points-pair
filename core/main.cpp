1dc5f033bbf0aa0cb5798016d5746593eec50095
Paul Zhu <zhufengminpaul@163.com>
2017-10-03 21:43:34 +0800

    add command line tools

--- core/main.cpp
+++ core/main.cpp
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include <iostream>
 #include <tuple>
 
@@ -5,41 +9,98 @@
 #include "solver.h"
 
 using std::tie;
-using std::cerr;
 using std::cout;
 
-int main()
+void print_usage(const char *name)
 {
-    int size = 1000000;
-    RandGen rg;
-    vector<Point> pts = rg.generate(size, 100000, 100000);
+    fprintf(stderr, "Usage: %s [-f file|-r size max_x max_y] [o|b|a]\n", name);
+    fprintf(stderr, "\t-f file        input points from `file`\n");
+    fprintf(stderr, "\t               file format: n x_1 y_1 x_2 y_2 ... x_n y_n\n");
+    fprintf(stderr, "\t-r size        generate `size` random points with maximum coordinate\n");
+    fprintf(stderr, "\t               (`max_x`, `max_y`)\n");
+    fprintf(stderr, "\to              solve with optimal algorithm (default)\n");
+    fprintf(stderr, "\tb              solve with brute-force algorithm\n");
+    fprintf(stderr, "\ta              solve with optimal and brute-force algorithm\n");
+}
 
+int main(int argc, char **argv)
+{
+    if (argc < 3) {
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    // input
+    vector<Point> pts;
+    int next;
+    if (strcmp(argv[1], "-f") == 0) {
+        next = 3;
+    } else if (strcmp(argv[1], "-r") == 0) {
+        if (argc < 5) {
+            print_usage(argv[0]);
+            return 1;
+        }
+        int size;
+        double max_x, max_y;
+        sscanf(argv[2], "%d", &size);
+        sscanf(argv[3], "%lf", &max_x);
+        sscanf(argv[4], "%lf", &max_y);
+
+        RandGen rg;
+        pts = rg.generate(size, max_x, max_y);
+        next = 5;
+    } else {
+        fprintf(stderr, "Invalid option: %s\n", argv[1]);
+        return 1;
+    }
+
+    // algorithm selection
+    int run_bf = 0;
+    int run_opt = 0;
+    if (next == argc) {
+        run_opt = 1;
+    } else {
+        if (strcmp(argv[next], "o") == 0) {
+            run_opt = 1;
+        } else if (strcmp(argv[next], "b") == 0) {
+            run_bf = 1;
+        } else if (strcmp(argv[next], "a") == 0) {
+            run_opt = 1;
+            run_bf = 1;
+        } else {
+            fprintf(stderr, "Invalid argument: %s\n", argv[next]);
+            return 1;
+        }
+    }
+
+    // run algorithm
     clock_t start, end;
     Point p1, p2;
     double d;
     tuple<Point, Point> ps;
+    double t;
 
-    double time1;
-    if (size <= 50000) {
+    if (run_opt) {
         start = clock();
-        NaiveSolver nsolver(pts);
-        tie(ps, d) = nsolver.solve();
+        Solver solver(pts);
+        tie(ps, d) = solver.solve();
         end = clock();
         tie(p1, p2) = ps;
-        cerr << p1 << " -> " << p2 << " : " << d << "\n";
-        time1 = (double) (end-start) / CLOCKS_PER_SEC * 1000.0;
+        t = (double) (end - start) / CLOCKS_PER_SEC * 1000.0;
+        cout << "Optimal: closest points pair " << p1 << " and " << p2 << ", distance " << d
+             << "\nOptimal: time (ms): " << t << "\n";
     }
 
-    start = clock();
-    Solver solver(pts);
-    tie(ps, d) = solver.solve();
-    end = clock();
-    tie(p1, p2) = ps;
-    cerr << p1 << " -> " << p2 << " : " << d << "\n";
-    double time2 = (double) (end-start) / CLOCKS_PER_SEC * 1000.0;
-
-    cerr << "naive=" << time1 << "ms\n";
-    cerr << "opt=" << time2 << "ms\n";
+    if (run_bf) {
+        start = clock();
+        NaiveSolver solver(pts);
+        tie(ps, d) = solver.solve();
+        end = clock();
+        tie(p1, p2) = ps;
+        t = (double) (end - start) / CLOCKS_PER_SEC * 1000.0;
+        cout << "Brute-force: closest points pair " << p1 << " and " << p2 << ", distance " << d
+             << "\nBrute-force: time (ms): " << t << "\n";
+    }
 
     return 0;
 }
---
+++ stat/stat.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+# running
+
+import os
+
+FROM = 10000
+TO = 100000
+STEP = 10000
+BOUND = 50000
+MAX_X = 10000
+MAX_Y = 10000
+
+ns = range(FROM, TO, STEP)
+ts_bf = []
+ts_opt = []
+
+for n in ns:
+    arg = ''
+    if n <= BOUND:
+        arg = 'a'
+    else:
+        arg = 'o'
+        ts_bf.append(-1)
+    output = os.popen('../core/closest -r %d %f %f %s' % (n, MAX_X, MAX_Y, arg))
+    lines = output.read().split('\n')
+    for line in lines:
+        if line.startswith('Brute-force: time (ms): '):
+            ts_bf.append(float(line[24:]))
+        elif line.startswith('Optimal: time (ms): '):
+            ts_opt.append(float(line[20:]))
+
+# table
+with open('table.tex', 'w+') as f:
+    f.write("""\\begin{tabular}{rll}
+\\toprule
+\\multirow{2}{*}{Number of points} & \\multicolumn{3}{c}{Run time (ms)} \\\\
+& Brute-force & Optimal \\\\
+\\midrule
+""")
+    for X in zip(ns, ts_bf, ts_opt):
+        f.write('%d & %f & %f \\\\\n' % X)
+    f.write("""\\bottomrule
+\\end{tabular}
+    """)
+    f.close()
+    print('table.tex saved')
+
+# plot
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.xlabel('number of points')
+plt.ylabel('time elapsed (ms)')
+plt.axis([10000, 100000, 0, 14000])
+bf, = plt.plot(ns, ts_bf, '+b')
+opt, = plt.plot(ns, ts_opt, 'dg')
+plt.legend([bf, opt], ['Brute-force', 'Optimal'])
+fig = plt.gcf() # 'get current figure'
+fig.savefig('cpp.eps', format='eps', dpi=1000)
+print('cpp.eps saved')
+plt.close('all')
+
+# large scale
+ns = range(100000, 1100000, 100000)
+ts = []
+for n in ns:
+    output = os.popen('../core/closest -r %d %f %f' % (n, MAX_X, MAX_Y))
+    lines = output.read().split('\n')
+    for line in lines:
+        if line.startswith('Optimal: time (ms): '):
+            ts.append(float(line[20:]))
+
+print ts
+
+plt.xlabel('number of points')
+plt.ylabel('time elapsed (ms)')
+plt.plot(ns, ts, 'dg')
+fig = plt.gcf() # 'get current figure'
+fig.savefig('opt.eps', format='eps', dpi=1000)
+print('opt.eps saved')
+plt.close('all')
